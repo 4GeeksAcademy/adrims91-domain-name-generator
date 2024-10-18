83f64e1d410cca28ecc11a768dc6f45dd6ae8900
adrims91 <adrims91@gmail.com>
2024-10-18 18:02:09 +0000

    finish

--- src/app.js
+++ src/app.js
@@ -7,5 +7,18 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  const myFunction = (arr1, arr2, arr3) => {
+    for (let i = 0; i < arr1.length; i++) {
+      for (let x = 0; x < arr2.length; x++) {
+        for (let j = 0; j < arr3.length; j++) {
+          console.log(`${arr1[i]}${arr2[x]}${arr3[j]}.com`);
+        }
+      }
+    }
+  };
+  myFunction(pronoun, adj, noun);
 };
